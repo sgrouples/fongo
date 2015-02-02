0aca374e77d7d98d1535c9270eff32adcd72f5e6
William Delanoue <=>
2015-02-02 16:55:45 +0100

    fisrt version of 2.13 drivers, need more work

--- src/main/java/com/github/fakemongo/impl/ExpressionParser.java
+++ src/main/java/com/github/fakemongo/impl/ExpressionParser.java
@@ -679,7 +679,7 @@ public class ExpressionParser {
       case 2:
         return obj instanceof CharSequence;
       case 3:
-        return obj instanceof Object;
+        return isDBObjectButNotDBList(obj);
       case 4:
         return obj instanceof List;
       case 7:
