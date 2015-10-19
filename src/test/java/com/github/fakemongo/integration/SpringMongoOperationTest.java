f0a0cb58a5804e1726ebabc391c80da2a721414c
William Delanoue <=>
2015-10-19 14:13:55 +0200

    refs #147

--- src/test/java/com/github/fakemongo/integration/SpringMongoOperationTest.java
+++ src/test/java/com/github/fakemongo/integration/SpringMongoOperationTest.java
@@ -1,10 +1,13 @@
 package com.github.fakemongo.integration;
 
 import com.github.fakemongo.junit.FongoRule;
+import com.google.common.collect.Sets;
 import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
+import com.mongodb.WriteResult;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
 import java.util.UUID;
 import org.assertj.core.api.Assertions;
 import static org.junit.Assert.assertEquals;
@@ -16,6 +19,9 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.core.index.IndexInfo;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+import org.springframework.data.mongodb.core.query.Update;
 
 /**
  * User: william
@@ -58,4 +64,43 @@ public class SpringMongoOperationTest {
     Assertions.assertThat(indexedId).as("_id field is not indexedId").isTrue();
     Assertions.assertThat(indexedName).as("name field is not indexedId").isTrue();
   }
+
+
+  static class A {
+    public A(String id, Set<B> bs) {
+      this.id = id;
+      this.bs = bs;
+    }
+
+    String id;
+    Set<B> bs;
+  }
+
+  static class B {
+    public B(String reference, Set<String> ids) {
+      this.reference = reference;
+      this.ids = ids;
+    }
+
+    String reference;
+    Set<String> ids;
+  }
+
+  @Test
+  public void test_ids() {
+    // Given
+    B b = new B("ref", Sets.newHashSet("1", "2"));
+    B b2 = new B("ref2", null);
+    A a = new A("id", Sets.newHashSet(b, b2));
+    mongoOperations.insert(a);
+    String reference = "ref";
+    String idToPull = "1";
+
+    // When
+    final WriteResult writeResult = mongoOperations.updateFirst(query(where("_id").is(a.id).and("bs").elemMatch(where("reference").is(reference))), new Update().pull("bs.$.ids", idToPull), A.class);
+
+    // Then
+    Assertions.assertThat(writeResult.getN()).isEqualTo(1);
+  }
+
 }
