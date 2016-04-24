391e9551fcd5dae0f323eedf1cc98357baf3acec
Kollivakkam Raghavan (rkolliva) <rkolliva@cisco.com>
2016-04-23 23:35:14 -0700

    Add support for mongo lookup aggregate pipeline

---
+++ src/test/java/com/github/fakemongo/test/beans/AbstractTestBean.java
@@ -0,0 +1,50 @@
+package com.github.fakemongo.test.beans;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Objects;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+
+/**
+ * @author rkolliva
+ * @created 4/23/2016
+ */
+public abstract class AbstractTestBean {
+
+    @Id
+    @JsonProperty("_id")
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AbstractTestBean that = (AbstractTestBean) o;
+        return Objects.equal(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("AbstractTestBean{");
+        sb.append("id='").append(id).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
